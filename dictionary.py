fb31e10997561d7adde81d8160c9d1a687c7fd25
Vjyas <95133861+Vjyas@users.noreply.github.com>
2022-02-05 19:51:59 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+import operator
+test_str='geeksforgeeks'
+all_freq={}
+for i in test_str:
+    if i in all_freq:
+        all_freq[i]+=1
+    else:
+        all_freq[i]=1
+so_d=dict(sorted(all_freq.items(),key=operator.itemgetter(1),reverse=True))
+print('count of all chars in geeksforgeeks is:\n',str(so_d))
